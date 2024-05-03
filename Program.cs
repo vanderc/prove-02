c68310408a028c83b8c1c9c4eb5b429e78159b88
vanderc <167822234+vanderc@users.noreply.github.com>
2024-05-02 20:42:39 -0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using prove_02;
+
+Console.WriteLine("\n======================\nSorting\n======================");
+Sorting.Run();
+
+Console.WriteLine("\n======================\nStandardDeviation\n======================");
+StandardDeviation.Run();
+
+Console.WriteLine("\n======================\nSearch\n======================");
+Search.Run();
